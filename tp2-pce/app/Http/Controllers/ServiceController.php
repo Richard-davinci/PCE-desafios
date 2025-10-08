35ec9cc2b8c9d28a6804d7340fbe7bab2af4d66e
ricardo garcia <ricardogarci4@gmail.com>
2025-10-08 01:41:28 -0300

    paginador

--- tp2-pce/app/Http/Controllers/ServiceController.php
+++ tp2-pce/app/Http/Controllers/ServiceController.php
@@ -12,7 +12,10 @@ class ServiceController extends Controller
    */
   public function index()
   {
-    $services = Service::all();
+//    $services = Service::all();
+
+    $services = Service::paginate(3);
+
     return view('service.index', compact('services'));
   }
 
